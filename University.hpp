b6d0d2627d183e59d5636b6d5391c28ba7abaf86
lukasz-kukulka <qqla83@gmail.com>
2021-06-16 21:41:24 +0100

    Add enum class wit option menu

--- University.hpp
+++ University.hpp
@@ -4,6 +4,21 @@ class University {
 public:
     University();
 
+    enum class MenuOption {
+        BaseMenu = 0,
+        AddStudents,
+        ShowStudent,
+        SearchStudents,
+        Exit,
+        SearchBySurname,
+        SearchByPESEL,
+        SortBySurname,
+        SortByPESEL,
+        Back,
+        Delete
+    };
+
 private:
-    
+    void printMenu();
+    MenuOption menuOption { MenuOption::BaseMenu };
 };
