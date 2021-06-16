7127aa2f5a4c9a3ad37fc295618f9f159e041916
lukasz-kukulka <qqla83@gmail.com>
2021-06-16 21:32:02 +0100

    Add printMenu function

--- University.cpp
+++ University.cpp
@@ -1,5 +1,17 @@
 #include "University.hpp"
 
-University::University(){
-    
+#include <iostream>
+
+University::University() {
+
+}
+
+void University::printMenu() {
+    std::cout << " --------------------- \n";
+    std::cout << "|  STUDENTS DATABASE  |\n";
+    std::cout << " --------------------- \n";
+    std::cout << "1. Add student\n";
+    std::cout << "2. Show all students\n";
+    std::cout << "3. Search students \n";
+    std::cout << "4. Exit\n";
 }
