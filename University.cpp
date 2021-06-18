d3c01a882e457bf7bfb56000e86c55eba6733bcc
lukasz-kukulka <qqla83@gmail.com>
2021-06-18 05:32:08 +0100

    Add  menu engine class

--- University.cpp
+++ University.cpp
@@ -13,9 +13,29 @@ void University::printMenu() {
     std::cout << "1. Add student\n";
     std::cout << "2. Show all students\n";
     std::cout << "3. Search students \n";
-    std::cout << "4. Exit\n";
+    std::cout << "4. Exit\n\n";
 }
 
 University::MenuOption University::menuEngine() {
-    
+    std::cout << "Please insert you choice: ";
+    uint8_t choice = 0;
+    std::cin >> choice;
+    switch (static_cast<MenuOption>(choice))
+    {
+        case MenuOption::AddStudents : {
+
+        } break;
+        case MenuOption::ShowStudent : {
+
+        } break;
+        case MenuOption::SearchStudents : {
+
+        } break;
+        case MenuOption::Exit : {
+
+        } break;    
+        default:
+        break;
+    }
+    return static_cast<MenuOption>(choice);
 }
--- University.hpp
+++ University.hpp
@@ -6,16 +6,16 @@ public:
 
     enum class MenuOption {
         BaseMenu = 0,
-        AddStudents,
-        ShowStudent,
-        SearchStudents,
-        Exit,
-        SearchBySurname,
-        SearchByPESEL,
-        SortBySurname,
-        SortByPESEL,
-        Back,
-        Delete
+        AddStudents = 1,
+        ShowStudent = 2,
+        SearchStudents = 3,
+        Exit = 4,
+        SearchBySurname = 11,
+        SearchByPESEL = 12,
+        SortBySurname = 21,
+        SortByPESEL = 22,
+        Back = 100,
+        Delete = 200
     };
 
 private:
