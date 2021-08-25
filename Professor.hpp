04c21d71c7bf5b0758474dcf043cc97fc278bf27
lukasz-kukulka <qqla83@gmail.com>
2021-08-25 07:16:32 +0100

    Add print person in student class

--- Professor.hpp
+++ Professor.hpp
@@ -12,11 +12,7 @@ public:
 
     std::string getSalary() const override { return salary_; }
     size_t getSizeSalaryLimit() const { return sizeSalaryLimit_; }
-
-    //void printBorderTop() override;
     void printPerson() override;
-    //void printBorderBotton() override;
-    //size_t getBorderSize();
 
 private:
     std::string salary_ {};
--- Student.cpp
+++ Student.cpp
@@ -17,21 +17,13 @@ Student::Student(std::string name, std::string surname, std::string address, std
 // }
 
 void Student::printPerson() {
-    std::cout << ' ' << std::setfill('-') << std::setw (static_cast<int>(getBorderSize())) <<  ' ' << '\n';
-    std::cout << std::setfill(' ') << '|' << std::setw(static_cast<int>(sizeNameLimit_ / 2 - name_.size() / 2 + name_.size()))
-              << name_ << std::setw(static_cast<int>(sizeNameLimit_ / 2 - name_.size() / 2 + 1 - name_.size() % 2)) 
-              << '|' << std::setw(static_cast<int>(sizeSurnameLimit_ / 2 - surname_.size() / 2 + surname_.size())) 
-              << surname_ << std::setw(static_cast<int>(sizeSurnameLimit_ / 2 - surname_.size() / 2 + 1 - surname_.size() % 2))
-              << '|' << std::setw(static_cast<int>(sizeAddressLimit_ / 2 - address_.size() / 2 + address_.size()))
-              << address_ << std::setw(static_cast<int>(sizeAddressLimit_ / 2 - address_.size() / 2 + 1  - address_.size() % 2))
-              << '|' << std::setw(static_cast<int>(sizeIndexNumberLimit_ / 2 - indexNumber_.size() / 2 + indexNumber_.size()))
-              << salary_ << std::setw(static_cast<int>(sizeSalaryLimit_ / 2 - salary_.size() / 2 + 1 - salary_.size() % 2))
-              << '|' << std::setw(static_cast<int>(sizePeselNumberLimit_ / 2 - peselNumber_.size() / 2 + peselNumber_.size()))
-              << indexNumber_ << std::setw(static_cast<int>(sizeIndexNumberLimit_ / 2 - indexNumber_.size() / 2 + 1 - indexNumber_.size() % 2))
-              << '|' << std::setw(static_cast<int>(sizePeselNumberLimit_ / 2 - peselNumber_.size() / 2 + peselNumber_.size()))
-              << peselNumber_ << std::setw(static_cast<int>(sizePeselNumberLimit_ / 2 - peselNumber_.size() / 2 + 1 - peselNumber_.size() % 2))
-              << '|' << std::setw(static_cast<int>(sizeGenderLimit_ / 2 - convertPersonGender(gender_).size() / 2 + convertPersonGender(gender_).size() % 2 + convertPersonGender(gender_).size()))
-              << convertPersonGender(gender_) << std::setw(static_cast<int>(sizeGenderLimit_ / 2 - convertPersonGender(gender_).size() / 2 + 2)) << "|\n";
+        std::cout << "|" << generateCell(name_, sizeNameLimit_) << "|" 
+              << generateCell(surname_, sizeSurnameLimit_) << "|" 
+              << generateCell(address_, sizeAddressLimit_) << "|" 
+              << generateCell(peselNumber_, sizePeselNumberLimit_) << "|" 
+              << generateCell(salary_, sizeSalaryLimit_) << "|" 
+              << generateCell(indexNumber_, sizeIndexNumberLimit_) << "|" 
+              << generateCell(convertPersonGender(gender_), sizeGenderLimit_) << "|\n";
 }
 
 // void Student::printBorderBotton() {
