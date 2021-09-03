0cab383c1d5ec4b6704a2c4458c91f7326daa356
lukasz-kukulka <qqla83@gmail.com>
2021-09-04 00:02:59 +0100

    Add function argument

--- Command.hpp
+++ Command.hpp
@@ -1,6 +1,8 @@
 
+#include "Person.hpp"
+#include <vector>
 class Command {
 public:
     virtual ~Command() = default;
-    virtual void operator()() = 0;
+    virtual void operator()(std::vector<Person>*) = 0;
 };
