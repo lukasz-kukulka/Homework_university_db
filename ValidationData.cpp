cde8bcb3d3b136ccc35005d39d6f0ac86b337922
lukasz-kukulka <qqla83@gmail.com>
2021-09-06 02:07:55 +0100

    Add validation menu function

--- ValidationData.cpp
+++ ValidationData.cpp
@@ -1,1 +1,15 @@
 #include "ValidationData.hpp"
+
+#include <limits> //for numeric_limits
+#include <ios> // for std::streamsize
+#include <iostream>
+
+bool ValidationData::isCorrectMenuChoice(const size_t choosenValueToValid, const size_t sizeMenu) {
+    if (std::cin.fail() || choosenValueToValid < 1 || choosenValueToValid > sizeMenu) {
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "Incorrect value, please put number betwen 1 and " << sizeMenu << '\n';
+        return false;
+    }
+    return true;
+}
--- ValidationData.hpp
+++ ValidationData.hpp
@@ -1,6 +1,8 @@
-
+#pragma once
+#include <stdlib.h> // for size_t
 class ValidationData {
 public:
+    bool isCorrectMenuChoice(const size_t choosenValueToValid, const size_t sizeMenu);
 
 private:
 
