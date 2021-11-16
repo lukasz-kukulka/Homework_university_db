03bb0cba2ae87902cddd4f5ae70a969fd52d75fe
lukasz-kukulka <qqla83@gmail.com>
2021-11-16 19:59:16 +0000

    enum class menu in sort class

--- SortPerson.hpp
+++ SortPerson.hpp
@@ -4,9 +4,21 @@
 
 class SortPerson : public Command {
 public:
+    enum class SortMenu{
+        NoChoice,
+        SearchByName,
+        SearchBySurname,
+        SearchByAddress,
+        SearchByPeselNumber,
+        SearchByGender,
+        SearchByIndex,
+        SearchBySalary,
+        Back
+    };
     ~SortPerson() = default;
     void operator()(std::vector<std::shared_ptr<Person>>& person) override;
 
 private:
     int printSortMenu();
+
 };
